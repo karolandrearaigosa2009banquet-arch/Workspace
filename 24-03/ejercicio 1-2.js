122e137d8f00e7567c37dbc54cbf93991a83fba6
karolandrearaigosa2009banquet-arch <karolandrearaigosa2009banquet@gmail.com>
2026-03-24 21:13:17 +0000

    24-03

---
+++ 24-03/ejercicio 1-2.js
@@ -0,0 +1,9 @@
+var pais="Japon"
+var precio= 2000;
+var asistio= true;
+console.log(" el tipo de pais es :", typeof(pais)) ;
+console.log(" el tipo de precio es :", typeof(precio)) ;
+console.log(" el tipo de asistio es :", typeof(asistio)) ;
+
+var vacio = null 
+console.log("el tipo de vacio es:" , typeof (vacio))
---
+++ adivinanza/adivinanza.js
@@ -0,0 +1,16 @@
+let Number= 24;
+
+
+const prompt = require("prompt-sync")();
+
+let Num= prompt("18");
+numero = Number(numero); // convertir a número
+
+console.log("El número es:", 18); 
+
+let a=15;
+let b;
+process.stdin.once("data" , d=>{
+    b= d.toString();
+});
+console.log()
